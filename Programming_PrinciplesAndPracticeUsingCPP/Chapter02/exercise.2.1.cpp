e836073b56d011324234214d2e46fc66fdb00ecd
igor <i.matukin@gmail.com>
2018-03-23 23:54:17 +0300

    Add Chapter 2 exercises

---
+++ Programming_PrinciplesAndPracticeUsingCPP/Chapter02/exercise.2.1.cpp
@@ -0,0 +1,13 @@
+// 1. Change the program to output the two lines
+// Hello, programming!
+// Here we go!
+#include "std_lib_facilities.h"
+
+//------------------------------------------------------------------------------
+
+int main() // C++ programs start by executing the function main
+{
+    cout << "Hello, programming!\n"; // output "Hello, programming!"
+    cout << "Here we go!\n"; // output "Here we go!"
+    return 0;
+}
