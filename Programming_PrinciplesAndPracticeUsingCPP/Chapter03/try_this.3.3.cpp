e304388556ed4c9bfeec31584f6e86a37ab7f49d
igor <i.matukin@gmail.com>
2018-04-01 15:04:58 +0300

    Try This

--- Programming_PrinciplesAndPracticeUsingCPP/Chapter03/try_this.3.3.cpp
+++ Programming_PrinciplesAndPracticeUsingCPP/Chapter03/try_this.3.3.cpp
@@ -1,4 +1,6 @@
-// read name and age
+// Get the “name and age” example to run. Then, modify it to write out the age in months: read the input in years and
+// multiply (using the * operator) by 12. Read the age into a double to allow for children who can be very proud of
+// being five and a half years old rather than just five.
 #include "std_lib_facilities.h"
 
 int main()
--- Programming_PrinciplesAndPracticeUsingCPP/Chapter03/try_this.3.4.cpp
+++ Programming_PrinciplesAndPracticeUsingCPP/Chapter03/try_this.3.4.cpp
@@ -1,3 +1,8 @@
+// Get this little program to run. Then, modify it to read an int rather than a double. Note that sqrt() is not defined
+// for an int so assign n to a double and take sqrt() of that. Also, “exercise” some other operations. Note that for
+// ints / is integer division and % is remainder (modulo), so that 5/2 is 2 (and not 2.5 or 3) and 5%2 is 1. The
+// definitions of integer *, /, and % guarantee that for two positive ints a and b we have a/b * b + a%b == a.
+
 // Простая программа, демонстрирующая работу операторов
 #include "std_lib_facilities.h"
 
