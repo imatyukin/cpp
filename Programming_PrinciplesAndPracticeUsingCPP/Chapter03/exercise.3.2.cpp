6c40bae6540215f5518772363db4ac292da828ae
igor <i.matukin@gmail.com>
2018-04-08 12:26:52 +0300

    Add Chapter 3 exercises

---
+++ Programming_PrinciplesAndPracticeUsingCPP/Chapter03/exercise.3.2.cpp
@@ -0,0 +1,13 @@
+// 2. Write a program in C++ that converts from miles to kilometers. Your program should have a reasonable prompt for
+// the user to enter a number of miles. Hint: There are 1.609 kilometers to the mile.
+#include "std_lib_facilities.h"
+
+int main()
+{
+    double mi = 0;
+
+    cout << "Введите количество миль:\n";
+    cin >> mi;
+
+    cout << mi << " миль = " << mi * 1.609 << " километров.\n";
+}
