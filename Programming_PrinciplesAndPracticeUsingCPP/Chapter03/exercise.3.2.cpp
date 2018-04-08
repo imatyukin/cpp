406d927829b8d2c11a92ffaa44f8b1355050d05f
igor <i.matukin@gmail.com>
2018-04-08 12:31:32 +0300

    Complete collection of code fragments

--- Programming_PrinciplesAndPracticeUsingCPP/Chapter03/exercise.3.2.cpp
+++ Programming_PrinciplesAndPracticeUsingCPP/Chapter03/exercise.3.2.cpp
@@ -1,4 +1,4 @@
-// 2. Write a program in C++ that converts from miles to kilometers. Your program should have a reasonable prompt for
+// 2 Write a program in C++ that converts from miles to kilometers. Your program should have a reasonable prompt for
 // the user to enter a number of miles. Hint: There are 1.609 kilometers to the mile.
 #include "std_lib_facilities.h"
 
