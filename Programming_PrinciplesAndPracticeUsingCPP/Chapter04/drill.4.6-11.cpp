78b02137a918c969fa6372b6e29f5a07c80af1dd
igor <i.matukin@gmail.com>
2018-05-02 00:11:18 +0300

    Add Chapter 4 drill

--- Programming_PrinciplesAndPracticeUsingCPP/Chapter04/drill.4.6-11.cpp
+++ Programming_PrinciplesAndPracticeUsingCPP/Chapter04/drill.4.6-11.cpp
@@ -15,49 +15,87 @@
 // 10. Keep all the values entered (converted into meters) in a vector. At the end, write out those values.
 // 11. Before writing out the values from the vector, sort them (that’ll make them come out in increasing order).
 #include "std_lib_facilities.h"
+#include <sstream>
 
 int main() {
     vector<double> numbers;
     double number = 0;
+    double number_conv = 0;
+    string unit;
     double smallest = 0;
     double largest = 0;
+    double sum = 0;
+    int counter = 0;
     bool is_first = true;
+    bool is_valid = true;
 
-    cout << "Введите число или '|' для выхода из программы:\n";
-    while (cin >> number) {
+    cout << "Введите число и единицу измерения (cm, m, in, ft) или '|' для выхода:\n";
+    while (cin >> number >> unit) {
         char c;
         c = cin.peek();
 
         if (c == '|')
             break;
         else {
-            cout << "Число " << number << endl;
-            numbers.push_back(number);
+            if (unit == "cm") {
+                number_conv = number / 100;         // 1m = 100cm
+                is_valid = true;
+            } else if (unit == "m") {
+                number_conv = number;               // 1m
+                is_valid = true;
+            } else if (unit == "in") {
+                number_conv = number * 0.0254;      // 1in = 2.54cm
+                is_valid = true;
+            } else if (unit == "ft") {
+                number_conv = number * 0.0254 * 12; // 1ft = 12in
+                is_valid = true;
+            } else
+                is_valid = false;
 
-            if (is_first) {
-                smallest = number;
-                largest = number;
-                cout << "Наибольшее среди введённых" << endl;
-                cout << "Наименьшее среди введённых" << endl;
-                is_first = false;
-                continue;
+            if (is_valid) {
+                cout << number << unit << endl;
+                numbers.push_back(number_conv);
+                sum += number_conv;
+                ++counter;
+
+                if (is_first) {
+                    smallest = number_conv;
+                    largest = number_conv;
+                    cout << "Наибольшее среди введённых" << endl;
+                    cout << "Наименьшее среди введённых" << endl;
+                    is_first = false;
+                    continue;
+                }
+
+                for (int i = 0; i < numbers.size(); ++i) {
+                    if (number_conv < numbers[i])
+                        if (number_conv < smallest)
+                            smallest = number_conv;
+                    if (number_conv > numbers[i])
+                        if (number_conv > largest)
+                            largest = number_conv;
+                }
+
+                if (number_conv == smallest)
+                    cout << "Наименьшее среди введённых" << endl;
+                if (number_conv == largest)
+                    cout << "Наибольшее среди введённых" << endl;
             }
-
-            for (int i = 0; i < numbers.size(); ++i) {
-                if (number < numbers[i])
-                    if (number < smallest)
-                        smallest = number;
-                if (number > numbers[i])
-                    if (number > largest)
-                        largest = number;
+            else if (is_valid == false) {
+                cout << "Введена неправильная единица измерения" << endl;
+                is_valid = true;
             }
-
-            if (number == smallest)
-                cout << "Наименьшее среди введённых" << endl;
-            if (number == largest)
-                cout << "Наибольшее среди введённых" << endl;
         }
     }
+
+    cout << "Наименьшее введённое значение: " << smallest << " m" << endl;
+    cout << "Наибольшее введённое значение: " << largest << " m" << endl;
+    cout << "Количество введённых значений: " << counter << endl;
+    cout << "Сумма значений: " << sum << " m" << endl;
+    cout << "Введённые значения (преобразованные в метры): \n";
+    sort(numbers.begin(), numbers.end());
+    for (int i = 0; i < numbers.size(); ++i)
+        cout << numbers[i] << " m\n";
 
     return 0;
 }
