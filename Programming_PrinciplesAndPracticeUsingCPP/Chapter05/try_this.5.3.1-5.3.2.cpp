9a71792088949a78a4c5170685295e85192594a2
igor <i.matukin@gmail.com>
2018-05-26 10:49:11 +0300

    Try This

--- Programming_PrinciplesAndPracticeUsingCPP/Chapter05/try_this.5.3.1-5.3.2.cpp
+++ Programming_PrinciplesAndPracticeUsingCPP/Chapter05/try_this.5.3.1-5.3.2.cpp
@@ -12,8 +12,12 @@ int main()
     int s2 = area(6, 5);        // error: ; missing
     int s3 = area(3, 4);        // error: Int is not a type
     int s4 = area(2, 1);        // error: non-terminated character (' missing)
-
     cout << "s1 = " << s1 << "; " << "s2 = " << s2 << "; " << "s3 = " << s3 << "; " << "s4 = " << s4 << "." << endl;
+
+    int x0 = area(7, 8);        // error: undeclared function
+    int x1 = area(8, 9);        // error: wrong number of arguments
+    int x2 = area(7, 2);        // error: 1st argument has a wrong type
+    cout << "x0 = " << x0 << "; " << "x1 = " << x1 << "; " << "x2 = " << x2 << "." << endl;
 
     return 0;
 }
