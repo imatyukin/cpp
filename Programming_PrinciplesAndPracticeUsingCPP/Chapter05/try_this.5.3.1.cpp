93be3db9bdbc071aa39db8211e37bb8dfa11f24a
igor <i.matukin@gmail.com>
2018-05-26 10:34:44 +0300

    Try This

---
+++ Programming_PrinciplesAndPracticeUsingCPP/Chapter05/try_this.5.3.1.cpp
@@ -0,0 +1,26 @@
+// Try to compile those examples and see how the compiler responds.
+#include "std_lib_facilities.h"
+
+int area(int length, int width);        // calculate area of a rectangle
+
+int main()
+{
+    cout << "Площадь треугольника равна произведению половины основания треугольника (a) на его высоту (h)" << endl;
+    cout << "S = 0.5ah" << endl;
+
+    int s1 = area(7, 7);        // error: ) missing
+    int s2 = area(6, 5);        // error: ; missing
+    int s3 = area(3, 4);        // error: Int is not a type
+    int s4 = area(2, 1);        // error: non-terminated character (' missing)
+
+    cout << "s1 = " << s1 << "; " << "s2 = " << s2 << "; " << "s3 = " << s3 << "; " << "s4 = " << s4 << "." << endl;
+
+    return 0;
+}
+
+int area(int length, int width)
+{
+    int s = 0;
+    s = 0.5 * width * length;
+    return s;
+}
