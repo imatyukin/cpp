eaf96bff1e36d3039831cd8fe610cf051c146488
igor <i.matukin@gmail.com>
2018-05-26 23:14:43 +0300

    Try This

---
+++ Programming_PrinciplesAndPracticeUsingCPP/Chapter05/try_this.5.8.cpp
@@ -0,0 +1,12 @@
+// Our hexagon was regular with 2cm sides. Did we get that answer right? Just do the “back of the envelope” calculation.
+// Take a piece a paper and scribble on it. Don’t feel that’s beneath you. Many famous scientists have been greatly
+// admired for their ability to come up with an approximate answer using a pencil and the back of an envelope (or a
+// napkin). This is an ability — a simple habit, really — that can save us a lot of time and confusion.
+#include "std_lib_facilities.h"
+
+int main()
+{
+    int a = 2;                                  // a - сторона
+    double s = 3 * sqrt(3) * pow(a, 2) / 2;     // s - площадь правильного шестиугольника
+    cout << s << endl;
+}
