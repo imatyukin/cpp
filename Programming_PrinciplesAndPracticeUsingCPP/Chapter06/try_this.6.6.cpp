1085674bfecb370fa84005fc00ba1a5798f3fc05
igor <i.matukin@gmail.com>
2018-06-12 23:19:46 +0300

    Try This

--- Programming_PrinciplesAndPracticeUsingCPP/Chapter06/try_this.6.6.cpp
+++ Programming_PrinciplesAndPracticeUsingCPP/Chapter06/try_this.6.6.cpp
@@ -15,9 +15,9 @@ public:
     char kind;        // what kind of token
     double value;     // for numbers: a value
     Token(char ch)    // make a Token from a char
-            :kind(ch), value(0) { }
+        :kind(ch), value(0) { }
     Token(char ch, double val)     // make a Token from a char and a double
-            :kind(ch), value(val) { }
+        :kind(ch), value(val) { }
 };
 
 //------------------------------------------------------------------------------
@@ -28,21 +28,21 @@ Token get_token()    // read a token from cin
     cin >> ch;    // note that >> skips whitespace (space, newline, tab, etc.)
 
     switch (ch) {
-        //not yet   case ';':    // for "print"
-        //not yet   case 'q':    // for "quit"
-        case '(': case ')': case '+': case '-': case '*': case '/':
-            return Token(ch);        // let each character represent itself
-        case '.':
-        case '0': case '1': case '2': case '3': case '4':
-        case '5': case '6': case '7': case '8': case '9':
+//not yet   case ';':    // for "print"
+//not yet   case 'q':    // for "quit"
+    case '(': case ')': case '+': case '-': case '*': case '/':
+        return Token(ch);        // let each character represent itself
+    case '.':
+    case '0': case '1': case '2': case '3': case '4':
+    case '5': case '6': case '7': case '8': case '9':
         {
             cin.putback(ch);         // put digit back into the input stream
             double val;
             cin >> val;              // read a floating-point number
             return Token('8',val);   // let '8' represent "a number"
         }
-        default:
-            error("Bad token");
+    default:
+        error("Bad token");
     }
 }
 
@@ -60,17 +60,17 @@ double primary()     // read and evaluate a Primary
 {
     Token t = get_token();
     switch (t.kind) {
-        case '(':    // handle '(' expression ')'
+    case '(':    // handle '(' expression ')'
         {
             double d = expression();
             t = get_token();
             if (t.kind != ')') error("')' expected");
             return d;
         }
-        case '8':            // we use '8' to represent a number
-            return t.value;  // return the number's value
-        default:
-            error("primary expected");
+    case '8':            // we use '8' to represent a number
+        return t.value;  // return the number's value
+    default:
+        error("primary expected");
     }
 }
 //------------------------------------------------------------------------------
@@ -100,16 +100,16 @@ double expression()
     Token t = get_token();     // get the next token
     while(true) {
         switch(t.kind) {
-            case '+':
-                left += term();    // evaluate Term and add
-                t = get_token();
-                break;
-            case '-':
-                left -= term();    // evaluate Term and subtract
-                t = get_token();
-                break;
-            default:
-                return left;       // finally: no more + or -: return the answer
+        case '+':
+            left += term();    // evaluate Term and add
+            t = get_token();
+            break;
+        case '-':
+            left -= term();    // evaluate Term and subtract
+            t = get_token();
+            break;
+        default:
+            return left;       // finally: no more + or -: return the answer
         }
     }
 }
@@ -123,11 +123,11 @@ double term()
 
     while(true) {
         switch (t.kind) {
-            case '*':
-                left *= primary();
-                t = get_token();
-                break;
-            case '/':
+        case '*':
+            left *= primary();
+            t = get_token();
+            break;
+        case '/':
             {
                 double d = primary();
                 if (d == 0) error("divide by zero");
@@ -135,8 +135,8 @@ double term()
                 t = get_token();
                 break;
             }
-            default:
-                return left;
+        default:
+            return left;
         }
     }
 }
