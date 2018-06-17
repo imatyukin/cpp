b186fdab7c37732e37ffb63fc739ffbcad900dea
igor <i.matukin@gmail.com>
2018-06-17 11:50:41 +0300

    Add Chapter 6 exercises

--- Programming_PrinciplesAndPracticeUsingCPP/Chapter06/exercise.6.4.cpp
+++ Programming_PrinciplesAndPracticeUsingCPP/Chapter06/exercise.6.4.cpp
@@ -5,43 +5,43 @@
 class Name_value {
 public:
     string name;
-    int score;
-    Name_value(string names, int scores)
-        :name(names), score(scores) { }
+    int value;
+    Name_value(string names, int values)
+        :name(names), value(values) { }
 };
 
 int main(){
-    vector<Name_value> ns;
+    vector<Name_value> nv;
     vector<string> entered_twice;
     string name = "";
-    int score = 0;
+    int value = 0;
 
     cout << "Введите набор пар, состоящих из имени и значения (прекращение ввода NoName 0): " << endl;
-    while (cin >> name && name != "NoName" && cin >> score && score != 0) {
-        ns.push_back(Name_value(name, score));
+    while (cin >> name && name != "NoName" && cin >> value && value != 0) {
+        nv.push_back(Name_value(name, value));
     }
 
-    for (size_t i = 0; i < ns.size(); i++) {
-        for (size_t j = 0; j < ns.size(); j++) {
-            if (i != j && ns[i].name == ns[j].name) {    // skip when it finds itself
-                // check if ns[i].name is already on the list
+    for (size_t i = 0; i < nv.size(); i++) {
+        for (size_t j = 0; j < nv.size(); j++) {
+            if (i != j && nv[i].name == nv[j].name) {    // skip when it finds itself
+                // check if nv[i].name is already on the list
                 int m = 0;
                 for (size_t k = 0; k < entered_twice.size(); k++) {
-                    if (entered_twice[k] == ns[i].name) {
+                    if (entered_twice[k] == nv[i].name) {
                         m++;
                         cout << "ОШИБКА! Имя " << entered_twice[k] << " введено дважды." << endl;
                         exit(0);
                     }
                 }
                 if (m == 0)
-                    entered_twice.push_back(ns[i].name);
+                    entered_twice.push_back(nv[i].name);
             }
         }
     }
 
-    for (size_t index = 0; index < ns.size(); index++)
+    for (size_t index = 0; index < nv.size(); index++)
     {
-        cout << ns[index].name; cout << ", " << ns[index].score << endl;
+        cout << nv[index].name; cout << ", " << nv[index].value << endl;
     }
 
     return 0;
