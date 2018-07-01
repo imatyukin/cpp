54c64e15fcd3cd72964eafaf2023b3c1489c10ad
igor <i.matukin@gmail.com>
2018-07-01 15:18:00 +0300

    Add Chapter 7 drill

--- Programming_PrinciplesAndPracticeUsingCPP/Chapter07/drill.7.1-11.cpp
+++ Programming_PrinciplesAndPracticeUsingCPP/Chapter07/drill.7.1-11.cpp
@@ -29,8 +29,13 @@ struct Token {
     char kind;
     double value;
     string name;
-    Token(char ch) :kind(ch), value(0) { }
+
+    // Инициализирует kind символом ch
+    Token(char ch) :kind(ch) { }
+    // Инициализирует kind и value
     Token(char ch, double val) :kind(ch), value(val) { }
+    // Инициализирует kind и name
+    Token(char ch, string n) :kind(ch), name(n) { }
 };
 
 class Token_stream {
