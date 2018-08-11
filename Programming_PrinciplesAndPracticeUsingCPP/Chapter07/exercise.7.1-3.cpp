935ddfceff4651935a63b3cba655ad3397eb7a4d
igor <i.matukin@gmail.com>
2018-08-11 15:26:51 +0300

    Add Chapter 7 exercises

--- Programming_PrinciplesAndPracticeUsingCPP/Chapter07/exercise.7.1-3.cpp
+++ Programming_PrinciplesAndPracticeUsingCPP/Chapter07/exercise.7.1-3.cpp
@@ -5,7 +5,6 @@
 // distinguishes between constants and variables and check for it in set_value(). If you want to let the user define
 // constants (rather than just having pi and e defined as constants), you’ll have to add a notation to let the user
 // express that, for example, const pi = 3.14;.
-
 /*
  * Простой калькулятор
  *
