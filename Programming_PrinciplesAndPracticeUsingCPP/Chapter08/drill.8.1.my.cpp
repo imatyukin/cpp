6577f5f6a3165529a19ac7b8ecb4974bb050e174
igor <i.matukin@gmail.com>
2018-08-27 22:01:36 +0300

    Add Chapter 8 drill

---
+++ Programming_PrinciplesAndPracticeUsingCPP/Chapter08/drill.8.1.my.cpp
@@ -0,0 +1,12 @@
+#include "drill.8.1.my.h"
+#include "std_lib_facilities.h"
+
+void print_foo()
+{
+    cout << "Значение переменной foo: " << foo << '\n';
+}
+
+void print(int i)
+{
+    cout << "Значение переменной i: " << i << '\n';
+}
