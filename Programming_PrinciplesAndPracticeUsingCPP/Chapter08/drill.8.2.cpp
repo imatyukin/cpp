f392fde68ef618ca8b02d28aed03f69954c738d4
igor <i.matukin@gmail.com>
2018-08-27 22:28:34 +0300

    Add Chapter 8 drill

--- Programming_PrinciplesAndPracticeUsingCPP/Chapter08/drill.8.2.cpp
+++ Programming_PrinciplesAndPracticeUsingCPP/Chapter08/drill.8.2.cpp
@@ -20,17 +20,62 @@
 
 #include "std_lib_facilities.h"
 
+void swap_v(int, int);
+void swap_r(int& a, int& b);
+void swap_cr(const int&, const int&);
+
+int main()
+{
+    int x = 7;
+    int y = 9;
+
+    // swap_v(x, y);                        // replace ? by v, r, or cr
+    swap_r(x, y);
+    // swap_cr(x, y);
+    // swap_v(7, 9);
+    // swap_r(7, 9);
+    // swap_cr(7, 9);
+
+    cout << "x = " << x << " и y = " << y << '\n';
+
+    const int cx = 7;
+    const int cy = 9;
+
+    // swap_v(cx, cy);
+    // swap_r(cx, cy);
+    // swap_cr(cx, cy);
+    // swap_v(7.7, 9.9);
+    // swap_r(7.7, 9.9);
+    // swap_cr(7.7, 9.9);
+
+    cout << "cx = " << cx << " и cy = " << cy << '\n';
+
+    double dx = 7.7;
+    double dy = 9.9;
+
+    // swap_v(dx, dy);
+    // swap_r(dx, dy);
+    // swap_cr(dx, dy);
+    // swap_v(7.7, 9.9);
+    // swap_r(7.7, 9.9);
+    // swap_cr(7.7, 9.9);
+
+    cout << "dx = " << dx << " и dy = " << dy << '\n';
+
+    return 0;
+}
+
 void swap_v(int a, int b)
 {
-
+    int temp; temp = a, a = b; b = temp;
 }
 
 void swap_r(int& a, int& b)
 {
-
+    int temp; temp = a, a = b; b = temp;
 }
 
-int main()
-{
-    return 0;
-}
+// void swap_cr(const int& a, const int& b)
+// {
+//    int temp; temp = a, a = b; b = temp;
+// }
