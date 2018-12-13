11ed82a1f696676cffc90dee2ae69c26dad60622
igor <i.matukin@gmail.com>
2018-12-14 00:05:39 +0300

    Add Chapter 9 exercises

---
+++ Programming_PrinciplesAndPracticeUsingCPP/Chapter09/exercise.9.14.cpp
@@ -0,0 +1,5 @@
+// 14. Design and implement a Money class for calculations involving dollars and cents where arithmetic has to be
+// accurate to the last cent using the 4/5 rounding rule (.5 of a cent rounds up; anything less than .5 rounds down).
+// Represent a monetary amount as a number of cents in a long int, but input and output as dollars and cents, e.g.,
+// $123.45. Do not worry about amounts that don’t fit into a long int.
+
