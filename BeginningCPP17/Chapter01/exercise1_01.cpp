3959145d294be521e9fbd4d04d39a5e6567cc781
igor <i.matukin@gmail.com>
2019-01-03 02:14:35 +0300

    Exercises

---
+++ BeginningCPP17/Chapter01/exercise1_01.cpp
@@ -0,0 +1,9 @@
+// Exercise 1-1: Create, compile, link, and execute a program that will display the text "Hello World" on your screen.
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+
+    return 0;
+}
