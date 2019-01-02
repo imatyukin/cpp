b578af6d74d7e2ce8cafa32a839d8aeb76e105d2
igor <i.matukin@gmail.com>
2019-01-03 02:20:09 +0300

    Exercises

---
+++ BeginningCPP17/Chapter01/exercise1_02.cpp
@@ -0,0 +1,7 @@
+// Exercise 1-2: Create and execute a program that outputs your name on one line and your age on the next line.
+#include <iostream>
+
+int main()
+{
+    std::cout << "Igor\n39" << std::endl;
+}
