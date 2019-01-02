a92bb3765a4107362aa9a94c0430abf5ddcf0bec
igor <i.matukin@gmail.com>
2019-01-03 02:25:23 +0300

    Exercises

---
+++ BeginningCPP17/Chapter01/exercise1_03.cpp
@@ -0,0 +1,13 @@
+// Exercise 1-3: The following program produces several compiler errors. Find these errors and correct them so the
+// program can compile cleanly and run.
+// include <iostream>
+// Int main()
+// {
+//   std:cout << "Hello World" << std:endl
+// )
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+}
