fb05caca2bcf83f809d90d88d08fe464cf05b11e
igor <i.matukin@gmail.com>
2019-01-08 21:40:27 +0300

    Exercises

--- BeginningCPP17/Chapter02/soln2_01.cpp
+++ BeginningCPP17/Chapter02/soln2_01.cpp
@@ -2,3 +2,18 @@
 // Write a program that will compute the area of a circle. The program should prompt for the radius of the circle to be
 // entered from the keyboard, calculate the area using the formula area = pi * radius * radius, and then display the
 // result.
+#include <iostream>
+
+int main()
+{
+    const double pi { 3.141592653589793238 };
+
+    double radius {};
+
+    std::cout << "Enter the circle radius: ";
+    std::cin >> radius;
+
+    const double area {pi * radius * radius};
+
+    std::cout << "\nThe area of the circle is " << area << ".\n";
+}
