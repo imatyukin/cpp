4c5a61e7661be3aea6eb7d26e4c75292b15ad9c8
igor <i.matukin@gmail.com>
2019-02-21 22:56:44 +0300

    Exercises

---
+++ BeginningCPP17/Chapter04/soln4_01.cpp
@@ -0,0 +1,14 @@
+// Exercise 4-1.
+// Write a program that prompts for two integers to be entered and then uses an if-else statement to output a message
+// that states whether the integers are the same.
+#include <iostream>
+
+int main()
+{
+    int a, b;
+    std::cout << "Enter two integers: ";
+    std::cin >> a >> b;
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+
+    std::cout << (a < b ? "a is less than b." : (a == b ? "a is equal to b." : "a is greater than b."));
+}
