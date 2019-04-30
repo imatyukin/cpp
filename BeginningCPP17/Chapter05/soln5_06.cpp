acc2264cd346a2375490c6791cfbd77f8d45c870
igor <i.matukin@gmail.com>
2019-05-01 01:40:44 +0300

    Exercises

---
+++ BeginningCPP17/Chapter05/soln5_06.cpp
@@ -0,0 +1,30 @@
+// Exercise 5-6.
+// Create a vector<> container with elements containing the integers from 1 to an arbitrary upper bound entered by the
+// user. Output the elements from the vector that contain values that are not multiples of 7 or 13. Output them 10 on a
+// line, aligned in columns.
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<unsigned int> v;
+    unsigned int x{}, count{};
+
+    cout << "Enter arbitrary upper bound: ";
+    cin >> x;
+    for (unsigned int i = 1; i <= x; i++)
+        v.push_back(i);
+
+    for (auto i : v) {
+        if ((i % 7) != 0 && (i % 13) != 0) {
+            cout << i << '\t';
+            count++;
+        }
+        if (count == 10) {
+            cout << '\n';
+            count = 0;
+        }
+    }
+}
