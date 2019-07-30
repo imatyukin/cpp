39bd8d02a0e6836a15a4ef4eb26b99607f703497
igor <i.matukin@gmail.com>
2019-07-30 15:58:08 +0300

    Exercises

--- BeginningCPP17/Chapter07/soln7_02.cpp
+++ BeginningCPP17/Chapter07/soln7_02.cpp
@@ -14,17 +14,17 @@ int main() {
     }
 
     size_t max_length {};
-    for (const auto &d : dict) {
-        if (max_length < d.first.length()) max_length = d.first.length();
+    for (const auto &w : dict) {
+        if (max_length < w.first.length()) max_length = w.first.length();
     }
-    std::cout << "Number of distinct words are: " << dict.size() << "\n";
+
+    std::cout << "Number of distinct words are: " << dict.size() << std::endl;
 
     size_t count {};
     const size_t perline {3};
-
-    for (const auto &d : dict) {
-        std::cout << std::setw(max_length) << std::left << d.first
-                  << std::setw(4) << std::right << d.second << "  ";
+    for (const auto &w : dict) {
+        std::cout << std::setw(max_length) << std::left << w.first
+                  << std::setw(4) << std::right << w.second << "  ";
         if (!(++count % perline))
             std::cout << std::endl;
     }
