6014faa01c7a472817886ebb942d48288cb2d625
igor <i.matukin@gmail.com>
2019-08-13 23:43:36 +0300

    Exercises

--- BeginningCPP17/Chapter07/soln7_06.cpp
+++ BeginningCPP17/Chapter07/soln7_06.cpp
@@ -7,6 +7,7 @@
 #include <sstream>
 
 std::vector<std::string> words;
+std::vector<std::string> new_words;
 
 int main(int argc, char** argv) {
     std::cout << "Enter a text string of arbitrary length terminated by 'quit': " << std::endl;
@@ -28,7 +29,15 @@ int main(int argc, char** argv) {
               << std::endl;
     std::cin >> letters;
 
-    for (size_t i {}; i < words.size(); ++i) {
-        std::cout << words[i] << " ";
+    for (auto ch : letters) {
+        for (auto word : words) {
+            if (std::toupper(word[0]) == std::toupper(ch)) {
+                new_words.push_back(word);
+            }
+        }
+    }
+
+    for (size_t i {}; i < new_words.size(); ++i) {
+        std::cout << new_words[i] << " ";
     }
 }
